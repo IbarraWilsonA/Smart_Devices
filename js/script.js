611b6a0e3f4be85b6285258aa45991669d2420ff
IbarraWilsonA <alan2003ibarra@gmail.com>
2024-04-27 01:16:34 -0400

    first commit

---
+++ js/script.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+    $(".button").click(function(){
+        var value = $(this).attr("data-filter");
+        if (value == "all")
+        {
+            $(".filter").show("1000");
+        }
+        else
+        {
+             $(".filter").not("."+value).hide("10000");
+             $(".filter").filter("."+value).show("10000");
+        }
+        //add active class
+        $("ul .button").click(function(){
+            $(this).addClass('active').siblings().removeClass('active');
+        })
+    })
+    
+    $("#toggleModeBtn").click(function() {
+        $("body").toggleClass("dark-mode");
+        $("#modeIcon").text($("body").hasClass("dark-mode") ? "🌙" : "🌞");
+    });
+})
